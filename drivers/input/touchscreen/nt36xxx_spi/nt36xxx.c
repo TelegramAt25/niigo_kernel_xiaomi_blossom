0379b9a670f0018f09f1e310b769c290eefd076f
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-05-07 23:43:02 +0700

    touchscreen: nt36xxx: Fix minimum usleep_range same as maximum
    Change-Id: Ic6d9bc3603d701bf2718385e79d16b570693e841
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
@@ -525,7 +525,7 @@ void nvt_sw_reset_idle(void)
 	//---MCU idle cmds to SWRST_N8_ADDR---
 	nvt_write_addr(SWRST_N8_ADDR, 0xAA);
 
-	msleep(15);
+	usleep_range(15000, 16000);
 }
 
 /*******************************************************
@@ -578,7 +578,7 @@ int32_t nvt_clear_fw_status(void)
 		if (buf[1] == 0x00)
 			break;
 
-		usleep_range(10000, 10000);
+		usleep_range(10000, 11000);
 	}
 
 	if (i >= retry) {
@@ -614,7 +614,7 @@ int32_t nvt_check_fw_status(void)
 		if ((buf[1] & 0xF0) == 0xA0)
 			break;
 
-		usleep_range(10000, 10000);
+		usleep_range(10000, 11000);
 	}
 
 	if (i >= retry) {
@@ -661,7 +661,7 @@ int32_t nvt_check_fw_reset_state(RST_COMPLETE_STATE check_reset_state)
 			break;
 		}
 
-		usleep_range(10000, 10000);
+		usleep_range(10000, 11000);
 	}
 
 	return ret;
@@ -1525,7 +1525,7 @@ static int8_t nvt_ts_check_chip_ver_trim(void)
 			}
 		}
 
-		msleep(10);
+		usleep_range(10000, 11000);
 	}
 
 out:
@@ -1929,7 +1929,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 #endif
 
 	// need 10ms delay after POR(power on reset)
-	msleep(10);
+	usleep_range(10000, 11000);
 
 	//---check chip version trim---
 	ret = nvt_ts_check_chip_ver_trim();
