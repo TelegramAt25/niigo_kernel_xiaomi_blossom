97d45d9a0bd55c8ccc647dcd5acb46d28e1ce291
Vaisakh Murali <mvaisakh@statixos.com>
2021-04-10 10:22:11 +0530

    drivers: power: mtk-battery: Explicitly Set battery log level to 0
    * Suppress the retarded dumping on the driver.
    * TODO: Find a better way to do this
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- drivers/power/supply/mtk_battery.c
+++ drivers/power/supply/mtk_battery.c
@@ -3106,7 +3106,7 @@ int battery_init(struct platform_device *pdev)
 	gm = gauge->gm;
 	gm->fixed_bat_tmp = 0xffff;
 	gm->tmp_table = Fg_Temperature_Table;
-	gm->log_level = BMLOG_ERROR_LEVEL;
+	gm->log_level = 0;
 	gm->sw_iavg_gap = 3000;
 
 	init_waitqueue_head(&gm->wait_que);
