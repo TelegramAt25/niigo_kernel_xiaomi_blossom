32535d677e745257a0d43fe7e1268a75635335f5
jordon.jiang <jordon.jiang@mediatek.com>
2021-04-17 15:43:04 +0800

    [ALPS05548993] iommu: mediatek: drivers: turn_off ion_history
    turn_off/on ion_history according to the situation.
    
    MTK-Commit-Id: 928852cb7b718b7ab5ecec439a52b39090359688
    
    Change-Id: I024cab4e3172264bf34dcdc920692a80f92ce9cc
    Signed-off-by: jordon.jiang <jordon.jiang@mediatek.com>
    CR-Id: ALPS05548993
    Feature: IOMMU
    (cherry picked from commit a5333c7e47d6c98bd77eed907227d43ce6ab0e3c)

--- drivers/staging/android/mtk_ion/ion.c
+++ drivers/staging/android/mtk_ion/ion.c
@@ -601,7 +601,9 @@ struct ion_handle *__ion_alloc(struct ion_client *client, size_t len,
 			 (unsigned long)client, (unsigned long)handle);
 
 #ifdef CONFIG_MTK_ION
+#ifdef ION_HISTORY_RECORD
 	ion_history_count_kick(true, len);
+#endif
 #endif
 
 	return handle;
@@ -643,7 +645,9 @@ void ion_free_nolock(struct ion_client *client,
 	}
 	ion_handle_put_nolock(handle);
 #ifdef CONFIG_MTK_ION
+#ifdef ION_HISTORY_RECORD
 	ion_history_count_kick(false, 0);
+#endif
 #endif
 }
 
