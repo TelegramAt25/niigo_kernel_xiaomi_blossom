0cae40515b74a1554cb22cebc59646651550b64a
fukiame <fukiame@proton.me>
2024-08-16 21:14:58 +0700

    mt-plat: mboot_params: jdjkwjchrisvvdhsbx
    ../drivers/misc/mediatek/include/mt-plat/mboot_params.h:937:1: error: non-void function does not return a value [-Werror,-Wreturn-type]
    }
    !!:s^
    ../drivers/misc/mediatek/include/mt-plat/mboot_params.h:941:1: error: non-void function does not return a value [-Werror,-Wreturn-type]
    }
    !!:s^
    ../drivers/misc/mediatek/include/mt-plat/mboot_params.h:1011:20: error: redefinition of aee_rr_rec_hang_detect_timeout_count
    static inline void aee_rr_rec_hang_detect_timeout_count(
                       ^
    ../drivers/misc/mediatek/include/mt-plat/mboot_params.h:862:20: note: previous definition is here
    static inline void aee_rr_rec_hang_detect_timeout_count(unsigned int val)
                       ^
    3 errors generated.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/include/mt-plat/mboot_params.h
+++ drivers/misc/mediatek/include/mt-plat/mboot_params.h
@@ -934,10 +934,12 @@ static inline void aee_rr_rec_set_bit_pmic_ext_buck(int bit, int loc)
 
 static inline int aee_rr_init_thermal_temp(int num)
 {
+	return 0;
 }
 
 static inline int aee_rr_rec_thermal_temp(int index, s8 val)
 {
+	return 0;
 }
 
 static inline void aee_rr_rec_thermal_status(u8 val)
@@ -1005,11 +1007,6 @@ static inline void aee_rr_rec_cpu_dead_ktime(u64 val)
 }
 
 static inline void aee_rr_rec_cpu_post_dead_ktime(u64 val)
-{
-}
-
-static inline void aee_rr_rec_hang_detect_timeout_count(
-		unsigned int timeout)
 {
 }
 
