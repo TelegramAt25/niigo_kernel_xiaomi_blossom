7e6b0b62448e07979819c14c3176f487a1963f36
fukiame <fukiame@proton.me>
2024-08-19 09:41:13 +0700

    touchscreen: ft8006s_spi: disable LCT_TP_USB_PLUGIN
    ...what the fck is this for?
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/input/touchscreen/ft8006s_spi/focaltech_config.h
+++ drivers/input/touchscreen/ft8006s_spi/focaltech_config.h
@@ -195,7 +195,7 @@
 /*
  * enable tp usb plugin feature
  */
-#define LCT_TP_USB_PLUGIN                       1
+#define LCT_TP_USB_PLUGIN                       0
 
 /****************************************************/
 
