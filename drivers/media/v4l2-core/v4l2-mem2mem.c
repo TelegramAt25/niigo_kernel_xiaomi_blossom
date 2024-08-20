8e399b675401e58f25038f0e3308d8baffaa59dd
fukiame <fukiame@proton.me>
2024-08-20 15:38:24 +0700

    v4l2-core: dbisjxbdisbbxjshbwha
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/media/v4l2-core/v4l2-mem2mem.c
+++ drivers/media/v4l2-core/v4l2-mem2mem.c
@@ -555,7 +555,7 @@ int v4l2_m2m_prepare_buf(struct file *file, struct v4l2_m2m_ctx *m2m_ctx,
 	int ret;
 
 	vq = v4l2_m2m_get_vq(m2m_ctx, buf->type);
-	ret = vb2_prepare_buf(vq, buf);
+	ret = vb2_prepare_buf(vq, vdev->v4l2_dev->mdev, buf);
 	if (ret)
 		return ret;
 
