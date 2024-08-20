cecb18538e7b604bb61f0c9251432fd4e99eecbb
fukiame <fukiame@proton.me>
2024-08-20 10:09:04 +0700

    drivers/misc: mediatek: usb20: destroy debugging
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/usb20/musb_debug.h
+++ drivers/misc/mediatek/usb20/musb_debug.h
@@ -7,8 +7,13 @@
 #define __MUSB_LINUX_DEBUG_H__
 
 #define yprintk(facility, format, args...) \
-		pr_notice("[MUSB]%s %d: " format, \
-		__func__, __LINE__, ## args)
+({							\
+	do {						\
+		if (0)					\
+			printk(format, ##__VA_ARGS__);	\
+	} while (0);					\
+	0;						\
+})
 
 /* workaroud for redefine warning in usb_dump.c */
 #ifdef WARNING
@@ -27,17 +32,14 @@
 #define INFO(fmt, args...) yprintk(KERN_INFO, fmt, ## args)
 #define ERR(fmt, args...) yprintk(KERN_ERR, fmt, ## args)
 
-#define xprintk(level,  format, args...) do { \
-	if (_dbg_level(level)) { \
-		if (musb_uart_debug) {\
-			pr_notice("[MUSB]%s %d: " format, \
-				__func__, __LINE__, ## args); \
-		} \
-		else {\
-			pr_debug("[MUSB]%s %d: " format, \
-				__func__, __LINE__, ## args); \
-		} \
-	} } while (0)
+#define xprintk(level,  format, args...)		\
+({							\
+	do {						\
+		if (0)					\
+			printk(format, ##__VA_ARGS__);	\
+	} while (0);					\
+	0;						\
+})
 
 extern unsigned int musb_debug;
 extern unsigned int musb_debug_limit;
@@ -53,20 +55,14 @@ static inline int _dbg_level(unsigned int level)
 #endif
 #define DBG(level, fmt, args...) xprintk(level, fmt, ## args)
 
-#define DBG_LIMIT(FREQ, fmt, args...) do {\
-	static DEFINE_RATELIMIT_STATE(ratelimit, HZ, FREQ);\
-	static int skip_cnt;\
-	\
-	if (unlikely(!musb_debug_limit))\
-		DBG(0, fmt "<unlimit>\n", ## args);\
-	else { \
-		if (__ratelimit(&ratelimit)) {\
-			DBG(0, fmt ", skip_cnt<%d>\n", ## args, skip_cnt);\
-			skip_cnt = 0;\
-		} else\
-			skip_cnt++;\
-	} \
-} while (0)\
+#define DBG_LIMIT(FREQ, fmt, args...)			\
+({							\
+	do {						\
+		if (0)					\
+			printk(format, ##__VA_ARGS__);	\
+	} while (0);					\
+	0;						\
+})
 
 /* extern const char *otg_state_string(struct musb *); */
 extern int musb_init_debugfs(struct musb *musb)  __attribute__((weak));
