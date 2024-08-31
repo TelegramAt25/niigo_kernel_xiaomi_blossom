be6c113fff6b14ce17ca3ad2245762609e93156f
fukiame <fukiame@proton.me>
2024-08-31 07:55:46 +0700

    drivers/mmc: host: mediatek: ComboA: kill msdc_ldo_power logging
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/mmc/host/mediatek/ComboA/mt6765/msdc_cust.c
+++ drivers/mmc/host/mediatek/ComboA/mt6765/msdc_cust.c
@@ -120,26 +120,17 @@ void msdc_ldo_power(u32 on, struct regulator *reg, int voltage_mv, u32 *status)
 
 	if (on) { /* want to power on */
 		if (*status == 0) {  /* can power on */
-			/*Comment out to reduce log */
-			/* pr_notice("msdc power on<%d>\n", voltage_uv); */
 			(void)msdc_regulator_set_and_enable(reg, voltage_uv);
 			*status = voltage_uv;
-		} else if (*status == voltage_uv) {
-			pr_notice("msdc power on <%d> again!\n", voltage_uv);
 		} else {
-			pr_notice("msdc change<%d> to <%d>\n",
-				*status, voltage_uv);
 			regulator_disable(reg);
 			(void)msdc_regulator_set_and_enable(reg, voltage_uv);
 			*status = voltage_uv;
 		}
 	} else {  /* want to power off */
 		if (*status != 0) {  /* has been powerred on */
-			pr_notice("msdc power off\n");
 			(void)regulator_disable(reg);
 			*status = 0;
-		} else {
-			pr_notice("msdc not power on\n");
 		}
 	}
 #endif
