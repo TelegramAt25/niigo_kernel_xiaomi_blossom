d4fe688a78e84568512c9d4d42c20f84ef2ba2bc
fukiame <fukiame@proton.me>
2024-09-10 14:18:46 +0700

    touchscreen: hxchipset: fix misleading indentation
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/input/touchscreen/hxchipset/himax_debug.c
+++ drivers/input/touchscreen/hxchipset/himax_debug.c
@@ -645,8 +645,8 @@ static ssize_t himax_proc_register_write(char *buf, size_t len)
 			if (strcmp(HX_85XX_H_SERIES_PWON,
 				private_ts->chip_name) == 0 && cfg_flag == 0)
 				cfg_flag = 2;
-				g_core_fp.fp_register_write(reg_cmd,
-					byte_length, &w_data[0], cfg_flag);
+			g_core_fp.fp_register_write(reg_cmd,
+				byte_length, &w_data[0], cfg_flag);
 
 			}
 		break;
