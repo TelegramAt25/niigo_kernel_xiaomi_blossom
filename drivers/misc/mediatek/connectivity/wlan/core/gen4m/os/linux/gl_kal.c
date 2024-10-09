29659fd4fc8508fc278a5ec36745ad248ca79864
fukiame <fukiame@proton.me>
2024-10-09 15:22:40 +0700

    djwinzjsis

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -3963,7 +3963,7 @@ int hif_thread(void *data)
 		wlanAcquirePowerControl(prAdapter);
 
 		/* Handle Interrupt */
-		fgEnInt = (prGlueInfo->ulFlag | GLUE_FLAG_INT_BIT) != 0;
+		fgEnInt = (prGlueInfo->ulFlag != 0) | (GLUE_FLAG_INT_BIT != 0);
 		if (test_and_clear_bit(GLUE_FLAG_INT_BIT,
 				       &prGlueInfo->ulFlag) ||
 		    test_and_clear_bit(GLUE_FLAG_DRV_INT_BIT,
