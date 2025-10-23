75a0af8ea5bb8f03f45707ad966b8c3709792a06
fukiame <fukiame@proton.me>
2025-10-23 23:22:31 +0700

    fixup! UPSTREAM: tcp: provide earliest departure time in skb->tstamp
    Signed-off-by: fukiame <fukiame@proton.me>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -792,7 +792,7 @@ static inline u32 tcp_skb_timestamp(const struct sk_buff *skb)
 /* provide the departure time in us unit */
 static inline u64 tcp_skb_timestamp_us(const struct sk_buff *skb)
 {
-	return skb->skb_mstamp;
+	return div_u64(skb->skb_mstamp_ns, NSEC_PER_USEC);
 }
 
 
