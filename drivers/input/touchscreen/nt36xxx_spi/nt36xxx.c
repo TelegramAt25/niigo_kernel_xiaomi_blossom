LineageOS/android_kernel_xiaomi_sdm845

touchscreen: nt36xxx: Move MP_CTRLRAM to Config entirely
Change-Id: I5b6ef2e8e88fb78f202165046237ee31b948d3e6
Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -51,7 +51,7 @@ extern int32_t nvt_extra_proc_init(void);
 extern void nvt_extra_proc_deinit(void);
 #endif
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_MP_CTRLRAM
 extern int32_t nvt_mp_proc_init(void);
 extern void nvt_mp_proc_deinit(void);
 #endif
@@ -1722,7 +1722,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	}
 #endif
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_MP_CTRLRAM
 	ret = nvt_mp_proc_init();
 	if (ret != 0) {
 		NVT_ERR("nvt mp proc init failed. ret=%d\n", ret);
@@ -1751,7 +1751,7 @@ err_register_drm_notif_failed:
 	if (drm_unregister_client(&ts->notifier))
 		NVT_ERR("Error occurred while unregistering drm_notifier.\n");
 #endif
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_MP_CTRLRAM
 nvt_mp_proc_deinit();
 err_mp_proc_init_failed:
 #endif
@@ -1824,7 +1824,7 @@ static int32_t nvt_ts_remove(struct i2c_client *client)
 
 	pm_qos_remove_request(&ts->pm_qos_req);
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_MP_CTRLRAM
 	nvt_mp_proc_deinit();
 #endif
 #if NVT_TOUCH_EXT_PROC
@@ -1890,7 +1890,7 @@ static void nvt_ts_shutdown(struct i2c_client *client)
 		NVT_ERR("Error occurred while unregistering drm_notifier.\n");
 #endif
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_MP_CTRLRAM
 	nvt_mp_proc_deinit();
 #endif
 #if NVT_TOUCH_EXT_PROC

TelegramAt25/niigo_kernel_xiaomi_blossom

drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
@@ -64,7 +64,7 @@ extern int32_t nvt_extra_proc_init(void);
 extern void nvt_extra_proc_deinit(void);
 #endif
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_HOSTDL_SPI_MP_CTRLRAM
 extern int32_t nvt_mp_proc_init(void);
 extern void nvt_mp_proc_deinit(void);
 #endif
@@ -2036,7 +2036,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	}
 #endif
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_HOSTDL_SPI_MP_CTRLRAM
 	ret = nvt_mp_proc_init();
 	if (ret != 0) {
 		NVT_ERR("nvt mp proc init failed. ret=%d\n", ret);
@@ -2178,7 +2178,7 @@ uninit_lct_tp_gesture();
 #endif
 err_init_lct_tp_info_failed:
 uninit_lct_tp_info();
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_HOSTDL_SPI_MP_CTRLRAM
 nvt_mp_proc_deinit();
 err_mp_proc_init_failed:
 #endif
@@ -2279,7 +2279,7 @@ static int32_t nvt_ts_remove(struct spi_device *client)
 #endif
 	uninit_lct_tp_info();
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_HOSTDL_SPI_MP_CTRLRAM
 	nvt_mp_proc_deinit();
 #endif
 #if NVT_TOUCH_EXT_PROC
@@ -2367,7 +2367,7 @@ static void nvt_ts_shutdown(struct spi_device *client)
 #endif
 	uninit_lct_tp_info();
 
-#if NVT_TOUCH_MP
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_HOSTDL_SPI_MP_CTRLRAM
 	nvt_mp_proc_deinit();
 #endif
 #if NVT_TOUCH_EXT_PROC